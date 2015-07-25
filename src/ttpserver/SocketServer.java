28fbb10111e862b4f377e0ceb27fc389bf1923b1
minhquan2106 <qgdgf@mst.edu>
2015-07-25 14:45:04 -0500

    Change byte[]

--- src/ttpserver/SocketServer.java
+++ src/ttpserver/SocketServer.java
@@ -72,24 +72,24 @@ public class SocketServer extends Thread {
                         DatabaseGetSet DB = new DatabaseGetSet();
                         
                         userG = DB.getG(userid);
-                        byte[] userGname = new byte['a'];
+                        byte[] userGname = {'a'};
                         byte[] userGByte = Add2Arrays(userGname, userG);
                         
                         userK = DB.getK(userid);
-                        byte[] userKname = new byte['b'];
+                        byte[] userKname = {'b'};
                         byte[] userKByte = Add2Arrays(userKname, userK);
                         
                         userGK = DB.getGK(userid);
-                        byte[] userGKname = new byte['c'];
+                        byte[] userGKname = {'c'};
                         byte[] userGKByte = Add2Arrays(userGKname, userGK);
                         
                         userZK = DB.getZK(userid);
-                        byte[] userZKname = new byte['d'];
+                        byte[] userZKname = {'d'};
                         byte[] userZKByte = Add2Arrays(userZKname, userZK);
                         
                         
                         userPrivateKey = DB.getPrivateKey(userid);
-                        byte[] userPKname = new byte['e'];
+                        byte[] userPKname = {'e'};
                         byte[] userPKByte = Add2Arrays(userPKname, userPrivateKey);
                         
                         userRole = DB.getRole(userid);

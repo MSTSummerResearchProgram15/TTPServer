86c3420a9c4e78c674fc902cf7d60059b752960a
minhquan2106 <qgdgf@mst.edu>
2015-07-25 13:23:56 -0500

    ahh

--- src/ttpserver/TTPServer.java
+++ src/ttpserver/TTPServer.java
@@ -18,15 +18,26 @@ public static void main(String[] args){
         //start the socket server and wait for incomming data
         SocketServer server = new SocketServer();
         System.out.println("Hello");
-        UserCreation newUser = new UserCreation();       
+        //UserCreation newUser = new UserCreation();       
         //newUser.delete(1);
-        ParamsGen PG = new ParamsGen();
-        Params p = new Params();
+        //ParamsGen PG = new ParamsGen();
+        //Params p = new Params();
         System.out.println("Get here");
-        p = PG.generate(160, 512);
+        //p = PG.generate(160, 512);
         System.out.println("Params is ok");
         //newUser.Register(2, "abc", 0, 128, p );
-        newUser.RegisterUser(2, "def");
+        //newUser.RegisterUser(2, "def");
         System.out.println("tada");
+        
+        
+        //FTP server socket
+        //FTPSocketServer soc=new FTPSocketServer();
+        //System.out.println("FTP Server Started on Port Number 5217");
+        //while(true)
+        //{
+        //    System.out.println("Waiting for Connection ...");
+            //transferfile t=new transferfile(soc.accept());
+            
+        //}
     }  
 }

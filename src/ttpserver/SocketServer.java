6815f2364d4ef9e8e6b904aef26f20b732acfd5a
NeedsMoreGravitas <dyshallow@gmail.com>
2015-07-25 17:48:45 -0500

    replaced flush

--- src/ttpserver/SocketServer.java
+++ src/ttpserver/SocketServer.java
@@ -136,17 +136,17 @@ public class SocketServer extends Thread {
                         
                         //Now send them to user
                         output.write(userGByte);
-                        output.flush();
+                        clientSoc.getOutputStream().flush();
                         output.write(userKByte);
-                        output.flush();
+                        clientSoc.getOutputStream().flush();
                         output.write(userGKByte);
-                        output.flush();
+                        clientSoc.getOutputStream().flush();
                         output.write(userZKByte);
-                        output.flush();
+                        clientSoc.getOutputStream().flush();
                         output.write(userPKByte);
-                        output.flush();
+                        clientSoc.getOutputStream().flush();
                         output.write(userRole);
-                        output.flush();
+                        clientSoc.getOutputStream().flush();
                         
                     }    
 
@@ -155,7 +155,7 @@ public class SocketServer extends Thread {
                     	String[] buffer = download.listFiles();
                     	for(int i = 0; i < buffer.length; i++){
                     		output2.write(buffer[i]);
-                                output.flush();
+                                clientSoc.getOutputStream().flush();
                     	}
                     }
                     
